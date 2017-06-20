5b30ee87884fd2e6db78195fd9d8d5716b00e250
paumendoza008 <paumendoza008@gmail.com>
2017-06-20 10:48:48 +0200

    Definición de cosas que quedan pendientes por hacer

--- apagar.sh
+++ apagar.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# TODO: Aquí se deberá hacer un chequeo 
+
 for i in $(cat $1) ; do
+	#TODO: generar un aviso informando de lo que se está haciendo
 	ssh root@$i init 0
 done;
+
+# TODO: generar un resumen informando de lo realizado
